596ed38de5faa6f50282ffefe2af36cd4a2f90ce
Zesty Alex <alextknott@gmail.com>
2021-12-28 22:59:44 +0000

    Added event listeners to all buttons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import { pageload } from "./pageload";
 pageload()
+
+const homeButton = document.getElementById("home")
+const contactButton = document.getElementById("contact")
+const menuButton = document.getElementById("menu")
+
+homeButton.addEventListener("click", () => {
+    console.log("hi")
+})
+
+contactButton.addEventListener("click", () => {
+    console.log("hi")
+})
+
+menuButton.addEventListener("click", () => {
+    console.log("hi")
+})
