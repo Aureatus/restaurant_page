18a10257c100e41bedcdefd91b5d809e80a184d2
Zesty Alex <alextknott@gmail.com>
2021-12-29 15:53:27 +0000

    Finished menu page

--- src/menupage.js
+++ src/menupage.js
@@ -1,18 +1,40 @@
 const menupageload = () => {
     let h1 = document.createElement("h1")
-    h1.textContent = "LOREM IPSUM DOLOR"
-    let img = document.createElement("img")
-    img.src = "https://i.picsum.photos/id/247/950/450.jpg?hmac=62HUPh9vGidtkQo9ER5Mp4Zjh0ugq6E8lapTZcNZTIk"
-    img.alt = "Lorem Ipsum Image."
-    let p = document.createElement("p")
-    p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod, ligula et finibus aliquam, eros felis bibendum nunc, sed faucibus neque dolor at orci. Nunc aliquam vestibulum nibh, eget sodales dolor commodo nec. Duis vel ante at sem molestie ullamcorper. Suspendisse potenti. Curabitur pulvinar sem eu tellus suscipit, dapibus blandit orci vulputate. Nunc blandit diam sit amet vehicula iaculis. Nulla malesuada fermentum ipsum, sit amet pretium risus commodo non. Fusce suscipit nibh in quam pharetra, vulputate eleifend dui mattis. Donec eleifend faucibus risus, eu viverra nisi porttitor nec. Pellentesque sed erat vulputate, dictum orci vel, tempus sem"
-    p.classList.add("copy")
+    h1.textContent = "MENU"
+    let milkshakeHeader = document.createElement("h2")
+    milkshakeHeader.textContent = "Mint milkshake"
+    let yoghurtHeader = document.createElement("h2")
+    yoghurtHeader.textContent = "Mint yoghurt"
+    let icecreamHeader = document.createElement("h2")
+    icecreamHeader.textContent = "Mint icecream"
+    let milkshakeImg = document.createElement("img")
+    milkshakeImg.src = "https://images.unsplash.com/photo-1586985288206-3cdc4f67cd03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80"
+    milkshakeImg.alt = "Image of a mint milkshake."
+    milkshakeImg.width = "300"
+    milkshakeImg.height = "400"
+    let yoghurtImg = document.createElement("img")
+    yoghurtImg.src = "https://images.unsplash.com/photo-1555791902-2bd22de5cd9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    yoghurtImg.alt = "Image of a mint yoghurt."
+    yoghurtImg.width = "300"
+    yoghurtImg.height = "400"
+    let icecreamImg = document.createElement("img")
+    icecreamImg.src = "https://images.unsplash.com/photo-1534706936160-d5ee67737249?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    icecreamImg.alt = "Image of mint icecream."
+    icecreamImg.width = "300"
+    icecreamImg.height = "400"
+    let imgCredit = document.createElement("p")
+    imgCredit.textContent = "Images from unsplashed.com"
     let content = document.getElementById("content")
     let header = document.querySelector("header")
     content.appendChild(header)
     content.appendChild(h1)
-    content.appendChild(img)
-    content.appendChild(p)
+    content.appendChild(milkshakeHeader)
+    content.appendChild(milkshakeImg)
+    content.appendChild(yoghurtHeader)
+    content.appendChild(yoghurtImg)
+    content.appendChild(icecreamHeader)
+    content.appendChild(icecreamImg)
+    content.appendChild(imgCredit)
     document.getElementById("home").style.backgroundColor = ""
     document.getElementById("contact").style.backgroundColor = ""
     document.getElementById("menu").style.backgroundColor = "#ACFCCC"
