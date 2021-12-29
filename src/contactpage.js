282f87187e4ff0bb872c3ff775b995ee01650b33
Zesty Alex <alextknott@gmail.com>
2021-12-29 17:47:36 +0000

    Added an email and a phone number to contact information.

--- src/contactpage.js
+++ src/contactpage.js
@@ -3,6 +3,12 @@ const contactpageload = () => {
     h1.textContent = "CONTACT US"
     let content = document.getElementById("content")
     content.appendChild(h1)
+    let emailText = document.createElement("h3")
+    emailText.textContent = "Email: placeholderemail@gmail.com"
+    content.appendChild(emailText)
+    let phoneText = document.createElement("h3")
+    phoneText.textContent = "Phone number: 01234 56789"
+    content.appendChild(phoneText)
     document.getElementById("home").style.backgroundColor = ""
     document.getElementById("menu").style.backgroundColor = ""
     document.getElementById("contact").style.backgroundColor = "#ACFCCC"
