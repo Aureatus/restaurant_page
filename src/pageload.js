013014fcb3335eba15c25ceec2f32fe9389fa709
Zesty Alex <alextknott@gmail.com>
2021-12-29 14:11:28 +0000

    Condensed code by using the homepage module at start

--- src/pageload.js
+++ src/pageload.js
@@ -1,12 +1,5 @@
+import { homepageload } from "./homepage"
 const pageload = () => {
-    let h1 = document.createElement("h1")
-    h1.textContent = "LOREM IPSUM DOLOR"
-    let img = document.createElement("img")
-    img.src = "https://i.picsum.photos/id/247/950/450.jpg?hmac=62HUPh9vGidtkQo9ER5Mp4Zjh0ugq6E8lapTZcNZTIk"
-    img.alt = "Lorem Ipsum Image."
-    let p = document.createElement("p")
-    p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod, ligula et finibus aliquam, eros felis bibendum nunc, sed faucibus neque dolor at orci. Nunc aliquam vestibulum nibh, eget sodales dolor commodo nec. Duis vel ante at sem molestie ullamcorper. Suspendisse potenti. Curabitur pulvinar sem eu tellus suscipit, dapibus blandit orci vulputate. Nunc blandit diam sit amet vehicula iaculis. Nulla malesuada fermentum ipsum, sit amet pretium risus commodo non. Fusce suscipit nibh in quam pharetra, vulputate eleifend dui mattis. Donec eleifend faucibus risus, eu viverra nisi porttitor nec. Pellentesque sed erat vulputate, dictum orci vel, tempus sem"
-    p.classList.add("copy")
     let tabs = document.createElement("tabs")
     let home = document.createElement("button")
     home.id = "home"
@@ -22,10 +15,8 @@ const pageload = () => {
     content.appendChild(header)
     header.appendChild(tabs)
     tabs.append(home,menu,contact)
-    content.appendChild(h1)
-    content.appendChild(img)
-    content.appendChild(p)
     home.style.backgroundColor = "#ACFCCC"
+    homepageload()
 }
 
 export {pageload}
